da7173c6094a5500387bf202e1bff7cef531732c
Tim Kelly <tkelly3118@gmail.com>
2022-01-13 20:58:55 +0000

    Fixed React v6 routing issue and added stats to homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
 import { Navbar, Homepage, Exchanges, Cryptocurrencies, News, CryptoDetails } from './components';
@@ -14,28 +14,26 @@ const App = () => {
             <div className="main">
                 <Layout>
                 <div className="routes">
-          <Switch>
-            <Route exact path="/">
-              <Homepage />
-            </Route>
-            <Route exact path="/exchanges">
-              <Exchanges />
-            </Route>
-            <Route exact path="/cryptocurrencies">
-              <Cryptocurrencies />
-            </Route>
-            <Route exact path="/crypto/:coinId">
-              <CryptoDetails />
-            </Route>
-            <Route exact path="/news">
-              <News />
-            </Route>
-          </Switch>
-        </div>
-                </Layout>
+                    <Routes>
+                        <Route path="/*" element={<Homepage />} />
+                        <Route path="/exchanges" element={<Exchanges />} />
+                        <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                        <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+                        <Route path="/news" element={<News />} />
+                    </Routes>
+                </div>
+        </Layout>
+                <div className="footer">
+                    <Typography.Title  level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        CryptoNews <br />
+                        All rights reserved
+                    </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/news">News</Link>
+                </Space>
             </div>
-            <div className="footer">
-
             </div>
         </div>
     )
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { Link } from 'react-router-dom';
+
+const { Title } = Typography;
 
 const Homepage = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+        <Title level={2} className="heading">Global Crypto Stats</Title>
+        <Row gutter={[32, 32]}>
+            <Col span={12}><Statistic title="Total Cryptocurrencies" value='12' /></Col>
+            <Col span={12}><Statistic title="Total Exchanges" value='12' /></Col>
+            <Col span={12}><Statistic title="Total Market Cap:" value='12' /></Col>
+            <Col span={12}><Statistic title="Total 24h Volume" value='12' /></Col>
+            <Col span={12}><Statistic title="Total Cryptocurrencies" value='12' /></Col>
+            <Col span={12}><Statistic title="Total Markets" value='12' /></Col>
+      </Row>
+        </>
     )
 }
 

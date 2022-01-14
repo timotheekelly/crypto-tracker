20902ab57ff5963324521859c9f07498834b0aaa
Tim Kelly <tkelly3118@gmail.com>
2022-01-14 21:31:18 +0000

    homepage api stuff

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Card, Row, Col, Input } from 'antd';
 
-const Cryptocurrencies = () => {
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
+const Cryptocurrencies = ({ simplified }) => {
+    
+    const count = simplified ? 10 : 100;
+    const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    if(isFetching) return 'Loading...';
+
     return (
-        <div>
-            Cryptocurrencies
-        </div>
+        <>
+            <Row gutter={[32, 32]} className='crypto-card-container'>
+                {cryptos?.map((currency) => (
+                    <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+                        <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
+                            <Card
+                                title={`${currency.rank}. ${currency.name}`}
+                                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                                hoverable
+                            >
+                                <p>Price: {millify(currency.price)}</p>
+                                <p>Market Cap: {millify(currency.marketCap)}</p>
+                                <p>Daily Change: {currency.change}%</p>
+                            </Card>
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
 
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,9 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-import { cryptoApi } from '../services/cryptoApi';
+// Note: Change v1 to v2 on rapid api
 
-export default configureStore({
-  reducer: {
-    [cryptoApi.reducerPath]: cryptoApi.reducer,
-  },
-});
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'ff72e12c32msh4d38939c8295762p1edd2ajsn305259b884b2'
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi ({
+    reducerPath:'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`),
+        })
+    })
+})
+
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
